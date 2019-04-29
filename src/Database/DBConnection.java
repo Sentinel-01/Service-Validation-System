21bb3600fcd69d177f31275fe0006faef98709e4
Sentinel-01 <49045455+Sentinel-01@users.noreply.github.com>
2019-04-29 12:22:06 -0400

    Update DBConnection.java

--- src/Database/DBConnection.java
+++ src/Database/DBConnection.java
@@ -9,6 +9,10 @@ import Views.MultiLogin;
 
 public class DBConnection {
 	
+	/**
+ 	*  Get the connection to the MySQL server, where the database is stored
+	*  Select all existing usernames and passwords
+ 	*/
 	public ResultSet getConnection(String username, String password,String usertype) throws Exception{
 		try {
 			String driver = "com.mysql.jdbc.Driver";
